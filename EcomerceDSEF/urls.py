3d065fcecba832b507e26a96d2fb55692b951b17
bothardy <youshii14@gmail.com>
2024-01-24 18:10:03 +0100

    adding STATICS to server

--- EcomerceDSEF/urls.py
+++ EcomerceDSEF/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from store.views import home
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),  # Include your app's URLs
+    path('', home, name='home.html'),  # Add this line for the root path
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def home(request):
+    # Fetch the first product from the database (you can modify this logic based on your needs)
+    product = Product.objects.all()
+
+    return render(request, 'store/home.html', {'product': product})
