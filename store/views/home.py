5d5988e58956dedaef7afa35638b8df5c6d29da4
bothardy <youshii14@gmail.com>
2024-01-26 14:57:27 +0100

    Html modification

--- store/views/home.py
+++ store/views/home.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , redirect , HttpResponseRedirect
-from store.models.Product import Product
+from store.models.Product import Product,Category
 from django.views import View
 
 
@@ -8,6 +8,9 @@ from django.views import View
 def home(request):
     # Fetch the first product from the database (you can modify this logic based on your needs)
     product = Product.objects.all()
+    category = Category.objects.all()
 
-    return render(request, 'store/home.html', {'product': product})
+    return render(request, 'store/home.html', {'product': product, 'category': category})
+
+
 
