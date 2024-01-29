c90496068c192e3690dbe21f12d8385b06cc201c
bothardy <youshii14@gmail.com>
2024-01-29 20:17:00 +0100

    passing to mysql

--- EcomerceDSEF/settings.py
+++ EcomerceDSEF/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'EcomerceDSEF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecomerce',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
+
 
 
 # Password validation
